3b07cbb4d8efb56e157a9b27f9aa7b683232eddf
Rishipreetham lingampally <61833673+Rishipreetham@users.noreply.github.com>
2022-06-19 15:37:17 +0530

    Add files via upload

---
+++ Pattern1.java
@@ -0,0 +1,305 @@
+public class Pattern1 {
+    public static void main(String args[])
+    {
+        //I
+
+        for(int i=0;i<5;i++)
+        {
+            for(int j=0;j<5;j++)
+            {
+                if(j==0)
+                System.out.print("*"+" ");
+                else
+                    System.out.print("  ");
+
+            }
+            System.out.println(" ");
+        }
+        System.out.println(" ");
+        System.out.println(" ");
+
+        // N
+
+        for(int i=0;i<5;i++)
+        {
+            if(i==0||i==4)
+            {
+                for(int j=0;j<5;j++)
+                {
+                    if(j==0||j==4)
+                    {
+                        System.out.print("*"+" ");
+                    }
+                    else
+                    {
+                        System.out.print("  ");
+                    }
+
+                }
+                System.out.println(" ");
+            }
+
+            else if (i==1)
+            {
+                for(int j=0;j<5;j++)
+                {
+                    if(j==0||j==1||j==4)
+                    {
+                        System.out.print("*"+" ");
+                    }
+                    else
+                    {
+                        System.out.print("  ");
+                    }
+
+                }
+                System.out.println(" ");
+            }
+            else if (i==2)
+            {
+                for(int j=0;j<5;j++)
+                {
+                    if(j==0||j==2||j==4)
+                    {
+                        System.out.print("*"+" ");
+                    }
+                    else
+                    {
+                        System.out.print("  ");
+                    }
+
+                }
+                System.out.println(" ");
+            }
+            else if (i==3)
+            {
+                for(int j=0;j<5;j++)
+                {
+                    if(j==0||j==3||j==4)
+                    {
+                        System.out.print("*"+" ");
+                    }
+                    else
+                    {
+                        System.out.print("  ");
+                    }
+
+                }
+                System.out.println(" ");
+            }
+        }
+
+        System.out.println(" ");
+        System.out.println(" ");
+
+        //E
+
+        for(int i=0;i<5;i++)
+        {
+            if(i==0||i==2||i==4)
+            {
+                for(int j=0;j<5;j++)
+                {
+                    System.out.print("*"+" ");
+                }
+                System.out.println(" ");
+            }
+            else
+            {
+                for(int j=0;j<5;j++)
+                {
+                    if(j==0)
+                    {
+                        System.out.print("*"+" ");
+                    }
+                    else
+                    {
+                        System.out.print("  ");
+                    }
+                }
+                System.out.println(" ");
+            }
+        }
+        System.out.println(" ");
+        System.out.println(" ");
+
+        //U
+
+
+        for (int i = 0; i < 5; i++) {
+            if (i == 4) {
+                for (int j = 0; j < 5; j++) {
+                    System.out.print("*" + " ");
+                }
+            } else {
+                for (int j = 0; j < 5; j++) {
+                    if (j == 0 || j == 4) {
+                        System.out.print("*" + " ");
+                    } else {
+                        System.out.print("  ");
+                    }
+                }
+                System.out.println(" ");
+            }
+        }
+
+        System.out.println(" ");
+        System.out.println(" ");
+
+        //R
+        for(int i=0;i<5;i++) {
+            if(i==0)
+            {
+                for(int j=0;j<5;j++)
+                {
+                    System.out.print("*"+" ");
+                }
+                System.out.println(" ");
+            }
+            else if(i==1||i==3)
+            {
+                for(int j=0;j<5;j++)
+                {
+                    if(j==0||j==3)
+                    {
+                        System.out.print("*"+" ");
+                    }
+                    else
+                    {
+                        System.out.print("  ");
+                    }
+                }
+                System.out.println(" ");
+            }
+            else if (i==2)
+            {
+                for (int j=0;j<5;j++)
+                {
+                    if(j==0||j==1||j==2)
+                    System.out.print("*"+" ");
+                    else
+                        System.out.print(" ");
+                }
+                System.out.println(" ");
+
+            }
+            else
+            {
+                for (int j=0;j<5;j++)
+                {
+                    if(j==0||j==4)
+                    System.out.print("*"+" ");
+                    else
+                        System.out.print("  ");
+                }
+                System.out.println(" ");
+            }
+        }
+        System.out.println(" ");
+        System.out.println(" ");
+
+        //O
+
+        for(int i=0;i<5;i++)
+        {
+            if(i==0 || i==4)
+            {
+                for (int j=0;j<5;j++)
+                {
+                    System.out.print("*"+" ");
+                }
+                System.out.println(" ");
+            }
+            else {
+                for(int j=0;j<5;j++)
+                {
+                    if(j==0||j==4)
+                    {
+                        System.out.print("*"+" ");
+                    }
+                    else {
+                        System.out.print("  ");
+                    }
+                }
+                System.out.println(" ");
+            }
+        }
+
+        System.out.println(" ");
+        System.out.println(" ");
+
+        //N
+
+        for(int i=0;i<5;i++)
+        {
+            if(i==0||i==4)
+            {
+                for(int j=0;j<5;j++)
+                {
+                    if(j==0||j==4)
+                    {
+                        System.out.print("*"+" ");
+                    }
+                    else
+                    {
+                        System.out.print("  ");
+                    }
+
+                }
+                System.out.println(" ");
+            }
+
+            else if (i==1)
+            {
+                for(int j=0;j<5;j++)
+                {
+                    if(j==0||j==1||j==4)
+                    {
+                        System.out.print("*"+" ");
+                    }
+                    else
+                    {
+                        System.out.print("  ");
+                    }
+
+                }
+                System.out.println(" ");
+            }
+            else if (i==2)
+            {
+                for(int j=0;j<5;j++)
+                {
+                    if(j==0||j==2||j==4)
+                    {
+                        System.out.print("*"+" ");
+                    }
+                    else
+                    {
+                        System.out.print("  ");
+                    }
+
+                }
+                System.out.println(" ");
+            }
+            else if (i==3)
+            {
+                for(int j=0;j<5;j++)
+                {
+                    if(j==0||j==3||j==4)
+                    {
+                        System.out.print("*"+" ");
+                    }
+                    else
+                    {
+                        System.out.print("  ");
+                    }
+
+                }
+                System.out.println(" ");
+            }
+        }
+
+
+
+    }
+}
